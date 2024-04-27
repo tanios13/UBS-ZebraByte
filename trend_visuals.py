c64a48c7267437698a31d04b65da86d5cde67074
Cyrus Achtari <148637933+cyrusachtari@users.noreply.github.com>
2024-04-27 23:08:47 +0200

    Update function trends

--- trend_visuals.py
+++ trend_visuals.py
@@ -9,9 +9,9 @@ def brand_level_trends(data, brand):
     compsets = data[data['business_entity_doing_business_as_name'] == brand]['compset'].unique()
     dates = data[(data['business_entity_doing_business_as_name'] == brand) & (data['compset'] == compsets[0])]['period_end_date'].unique()
 
-
-    trends_all = np.empty((len(compsets), 6, len(dates)))
-    trends_brand = np.empty((6, len(dates)))
+    trends_all = np.empty((len(compsets), 5, len(dates)))
+    trends_brand = np.empty((5, len(dates)))
+    trends_cumulative = np.zeros((trends_all.shape[1], trends_all.shape[2]))
 
     for i in range(len(compsets)):
         
@@ -23,7 +23,7 @@ def brand_level_trends(data, brand):
         trends_all[i, 2] = (subset_all['videos'] - subset_all['videos'].min()) / ((subset_all['videos'].max() - subset_all['videos'].min()))
         trends_all[i, 3] = (subset_all['pictures'] - subset_all['pictures'].min()) / ((subset_all['pictures'].max() - subset_all['pictures'].min()))
         trends_all[i, 4] = (subset_all['followers'] - subset_all['followers'].min()) / ((subset_all['followers'].max() - subset_all['followers'].min()))
-        trends_all[i, 5] = pd.to_datetime(subset_all['period_end_date']).astype(int) / 10**9
+        trends_cumulative += trends_all[i]
 
         if i == 0:
             subset_brand = data[(data['business_entity_doing_business_as_name'] == brand) & (data['compset'] == compsets[i]) & (data['period_end_date'].isin(dates)) & (data['compset_group'] == compset_group[0])]  # Ajoutez cette condition
@@ -34,17 +34,16 @@ def brand_level_trends(data, brand):
             trends_brand[2] = (subset_brand['videos'] - subset_brand['videos'].min()) / ((subset_brand['videos'].max() - subset_brand['videos'].min()))
             trends_brand[3] = (subset_brand['pictures'] - subset_brand['pictures'].min()) / ((subset_brand['pictures'].max() - subset_brand['pictures'].min()))
             trends_brand[4] = (subset_brand['followers'] - subset_brand['followers'].min()) / ((subset_brand['followers'].max() - subset_brand['followers'].min()))
-            trends_brand[5] = pd.to_datetime(subset_brand['period_end_date']).astype(int) / 10**9    
             
-    return trends_brand, trends_all
+    return trends_brand, trends_all, trends_cumulative/trends_all.shape[0], dates
 
-def plot_metrics_individual(trends_brand, trends_all):
+def plot_metrics_individual(trends_brand, trends_all, dates):
     metrics = ['likes', 'comments', 'videos', 'pictures', 'followers']
     fig, axs = plt.subplots(5, 1, figsize=(10, 10))
     
     for i in range(5):
-        axs[i].plot(pd.to_datetime(trends_brand[5], unit='s'), trends_brand[i], label=f'{metrics[i]} (brand)')
-        axs[i].plot(pd.to_datetime(trends_all[0, 5], unit='s'), trends_all[0, i], label=f'{metrics[i]} (all brands)')
+        axs[i].plot(dates, trends_brand[i], label=f'{metrics[i]} (brand)')
+        axs[i].plot(dates, trends_all[0, i], label=f'{metrics[i]} (all brands)')
         axs[i].set_title(f'Trends for {metrics[i]}')
         axs[i].set_xlabel('Date')
         axs[i].set_ylabel('Value')
@@ -55,14 +54,13 @@ def plot_metrics_individual(trends_brand, trends_all):
     plt.tight_layout()
     plt.show()
 
-def plot_metrics_total(trends_brand, trends_all):
+def plot_metrics_total(trends_brand, trends_cumulative, dates):
     metrics = ['likes', 'comments', 'videos', 'pictures', 'followers']
     fig, axs = plt.subplots(5, 1, figsize=(10, 10))  # Change figsize here
 
     for i in range(5):
-        trends_all_sum = np.sum(trends_all[:, i], axis=0)
-        axs[i].plot(pd.to_datetime(trends_brand[5], unit='s'), trends_brand[i], label=f'{metrics[i]} (brand)')
-        axs[i].plot(pd.to_datetime(trends_all[0, 5], unit='s'), trends_all_sum/trends_all.shape[0], label=f'{metrics[i]} (all brands)')
+        axs[i].plot(dates, trends_brand[i], label=f'{metrics[i]} (brand)')
+        axs[i].plot(dates, trends_cumulative[i], label=f'{metrics[i]} (all brands)')
 
         axs[i].set_title(f'Trends for {metrics[i]} cumulated over all compsets')
         axs[i].set_xlabel('Date')
